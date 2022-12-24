dfa022c3cffa6669fef252da6450081a93b147d9
Simon Cateau <sorani.dev@outlook.com>
2022-12-24 14:36:00 +0100

    Create app Musker, base HTML templates and routes

---
+++ social/musker/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'musker/home.html', {})
