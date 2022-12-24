205c8b165b1369eb257c810662776a7f20fda32f
Simon Cateau <sorani.dev@outlook.com>
2022-12-24 22:20:28 +0100

    feat: Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Django Wednesdays Twitter
+
+Youtube playlist (<https://youtube.com/playlist?list=PLCC34OHNcOtoQCR6K4RgBWNi3-7yGgg7b>) *by Codemy.com*
+
+## Syllabus
+
+1. Let's Build Twitter With Django and Python - Django Wednesdays Twitter
+2. #1 Creating Users, Profiles, & Follows - Django Wednesdays Twitter #2
+3. 
--- social/musker/models.py
+++ social/musker/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """User Profile Model"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True)
+
+    def __str__(self):
+        return self.__repr__();
+
+    def __repr__(self):
+        return self.user.username
