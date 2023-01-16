28284eeed58636d9fc516c2323800e677420f6f4
Simon Cateau <sorani.dev@outlook.com>
2023-01-16 19:05:13 +0100

    feat: Automatically Create User Profiles (admin area)

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Youtube playlist (<https://youtube.com/playlist?list=PLCC34OHNcOtoQCR6K4RgBWNi3-
 
 1. Let's Build Twitter With Django and Python - Django Wednesdays Twitter
 2. #1 Creating Users, Profiles, & Follows - Django Wednesdays Twitter #2
-3. 
+3. Automatically Create User Profiles - Django Wednesdays Twitter #3
--- social/musker/models.py
+++ social/musker/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import post_save
+ from django.dispatch import receiver
 
 class Profile(models.Model):
     """User Profile Model"""
@@ -16,3 +17,17 @@ class Profile(models.Model):
 
     def __repr__(self):
         return self.user.username
+
+
+# Create Profile when new User signs up
+@receiver(post_save, sender=User)
+def create_profile(sender: models.base.ModelBase, instance: User, created: bool, **kwargs) -> None:
+    print(type(sender), type(instance), type(created))
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+# post_save.connect(create_profile, sender=User)
