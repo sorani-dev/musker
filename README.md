7e24a8a4a4292ed25a248691ed89506fd4780290
Simon Cateau <sorani.dev@outlook.com>
2023-01-19 14:50:15 +0100

    feat: Follow and Unfollow users Buttons

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Youtube playlist (<https://youtube.com/playlist?list=PLCC34OHNcOtoQCR6K4RgBWNi3-
 3. Automatically Create User Profiles - Django Wednesdays Twitter #3
 4. List Profiles Page For Our Twitter App - Django Wednesdays Twitter #4
 5. Profile Page With Followers - Django Wednesdays Twitter #5
+6. Follow and Unfollow Buttons - Django Wednesdays Twitter #6
--- social/musker/views.py
+++ social/musker/views.py
@@ -22,7 +22,23 @@ def profile(request: HttpRequest, pk:int)->HttpResponse:
     View a Single profile
     """
     if request.user.is_authenticated:
+        # Get user profile
         profile = Profile.objects.get(user_id=pk)
+        # Perform form logic on follow/unfollow other user
+        if request.method == 'POST':
+            # Get the current logged in user's profile
+            current_user_profile:Profile = request.user.profile
+            # Get form data
+            action = request.POST.get('follow')
+            # Decide to follow or unfollow
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            # Save the profile
+            current_user_profile.save()
+        
+        # Render the profile view
         return render(request, 'musker/profile.html', {'profile': profile})
     messages.warning(request, ('You must be logged in to view this page'))
     return redirect('home')
